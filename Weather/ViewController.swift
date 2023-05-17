bbbb0fdb1f44df7254f930b4107daf8ce70b62e8
jinunzzan <lovebox6607@gmail.com>
2023-05-17 22:23:22 +0900

    api 연동 후 데이터 못 가져옴.

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityNameTextField: UITextField!
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    @IBOutlet weak var maxTempLabel: UILabel!
+    
+    @IBOutlet weak var minTempLabel: UILabel!
+    
+    @IBOutlet weak var weatherStackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
-}
+    @IBAction func tapFetchWeatherButton(_ sender: UIButton) {
+        if let cityName = self.cityNameTextField.text {
+            self.getCurrentWeather(cityName: cityName)
+            self.view.endEditing(true)
+            print("날씨가져오기 클릭")
+        }
+        
+    }
+    func configureView(weatherInformation: WeatherInformation){
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather = weatherInformation.weather.first{
+            self.weatherDescriptionLabel.text = weather.description
+        }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15))도"
+        self.minTempLabel.text = "최저: \(Int(weatherInformation.temp.tempMin - 273.15))도"
+        self.maxTempLabel.text = "최고: \(Int(weatherInformation.temp.tempMax  - 273.15))도"
+    }
+    
+    func getCurrentWeather(cityName: String){
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=b3457a7face33829ef9791932f896fc3") else {return}
+            let session = URLSession(configuration: .default)
+            session.dataTask(with: url) {[weak self] data, response, error in
+                guard let data = data, error == nil else {return}
+                let decoder = JSONDecoder()
+                guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else {return}
+                DispatchQueue.main.async {
+                    self?.weatherStackView.isHidden = false
+                    self?.configureView(weatherInformation: weatherInformation)
+                }
+                debugPrint(weatherInformation)
+            }.resume()
+        }
+
+    }
 
---
+++ Weather/WeatherInformation.swift
@@ -0,0 +1,45 @@
+//
+//  WeatherInformation.swift
+//  Weather
+//
+//  Created by Eunchan Kim on 2023/05/16.
+//
+
+import Foundation
+
+struct WeatherInformation: Codable{
+    //외부표현으로 변환해주는 코더블~
+    //디코더블과 인코더블
+    //WeatherInformation 형태로 디코딩
+    //api에서 필요한 프로퍼티만 데려옴
+    let weather: [Weather]
+    let temp: Temp
+    let name: String
+    
+    enum CodingKeys:String, CodingKey {
+        case weather
+        case temp = "main"
+        case name
+    }
+}
+
+struct Weather: Codable{
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+struct Temp: Codable{
+    //이름이 다를 때 매핑하기
+    let temp: Double
+    let feelsLike: Double
+    let tempMin: Double
+    let tempMax: Double
+    
+    enum codingKeys: String, CodingKey{
+        case temp
+        case feelsLike = "feels_like"
+        case minTemp = "temp_min"
+        case maxTemp = "temp_max"
+    }
+}
